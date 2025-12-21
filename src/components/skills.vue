e7d7a0eca32826ac95f11229967574549022ab54
5aled1801 <zx5aled1801@gmail.com>
2025-12-21 03:00:41 +0200

    go2

--- src/components/skills.vue
+++ src/components/skills.vue
@@ -7,56 +7,56 @@
         <v-row>
             <!-- Use `cols="6" md="4"` to control the number of columns on mobile vs desktop -->
             <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/javascript-logo-javascript-icon-transparent-free-png.webp"
+                <!-- <img src="./../assets/images/javascript-logo-javascript-icon-transparent-free-png.webp" -->
                     :style="{ maxWidth: '400px' }" class="mx-auto my-4" loading="lazy" />
                 <div class="mt-2">
                     <h4>JavaScript </h4>
                 </div>
             </v-col>
             <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/vue.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/vue.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Vue.js</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/html5_23329.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/html5_23329.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>HTML5</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/tailwend.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/tailwend.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Tailwend</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/aaaaaaaaaaa.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/aaaaaaaaaaa.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>CSS3</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/bbb.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/bbb.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Bootstrap</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/pinia.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/pinia.png" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Pinia</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/ssss.webp" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/ssss.webp" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Sass</h4>
                 </div>
             </v-col> <v-col class="text-center" cols="6" md="4">
-                <img src="./../assets/images/nuxt.webp" :style="{ maxWidth: '400px' }" class="mx-auto my-4"
+                <!-- <img src="./../assets/images/nuxt.webp" :style="{ maxWidth: '400px' }" class="mx-auto my-4" -->
                     loading="lazy" />
                 <div class="mt-2">
                     <h4>Nuxt</h4>
