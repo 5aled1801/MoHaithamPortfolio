c789c9c4f8ed1558e61ad02f7837d28255825e91
5aled1801 <zx5aled1801@gmail.com>
2025-12-21 02:57:10 +0200

    go

---
+++ src/App.vue
@@ -0,0 +1,260 @@
+<script>
+import myInfoCom from './components/myInfo.vue';
+import about from './components/about.vue';
+import skills from './components/skills.vue';
+import projects from './components/projects.vue';
+import contact from './components/contact.vue';
+
+export default {
+  data() {
+
+    return {
+      drawer: false,
+      vantaEffect: null,
+      icons: [
+        "mdi-facebook",
+        "mdi-snapchat",
+        "mdi-linkedin",
+        "mdi-github",
+        "mdi-instagram",
+      ]
+
+    };
+  },
+  components: {
+
+    myInfoCom,
+    about, skills, projects, contact
+  }
+  ,
+
+  mounted() {
+
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => {
+    //     console.log("Latitude:", position.coords.latitude);
+    //     console.log("Longitude:", position.coords.longitude);
+    //     console.log("lllll", position);
+
+    //   },
+    //   (error) => {
+    //     console.error("Error getting location:", error);
+    //   }
+    // );
+
+
+
+    this.vantaEffect = window.VANTA.TOPOLOGY({
+      el: document.getElementById("vanta"),
+      mouseControls: false,
+      touchControls: false,
+      gyroControls: false,
+      minHeight: 200.0,
+      minWidth: 200.0,
+      scale: 1.0,
+      scaleMobile: 1.0,
+      color: 0x1da4ff,
+      backgroundColor: 0x000000
+    },
+      this.view = true
+    );
+
+  },
+  beforeUnmount() {
+    if (this.vantaEffect) this.vantaEffect.destroy();
+  }
+};
+</script>
+
+
+<template>
+  <v-app style="background: none;">
+
+    <v-app-bar app color="black">
+      <!-- Logo -->
+      <v-toolbar-title class="d-flex align-center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="120" height="50" viewBox="0 0 400 140">
+          <defs>
+            <linearGradient id="gradFinalAnim" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop offset="0%" style="stop-color:#1da4ff">
+                <animate attributeName="stop-color" values="#1da4ff;#ff4081;#1da4ff" dur="4s"
+                  repeatCount="indefinite" />
+              </stop>
+              <stop offset="50%" style="stop-color:#00ffe7">
+                <animate attributeName="stop-color" values="#00ffe7;#ff00ff;#00ffe7" dur="4s"
+                  repeatCount="indefinite" />
+              </stop>
+              <stop offset="100%" style="stop-color:#ff4081">
+                <animate attributeName="stop-color" values="#ff4081;#1da4ff;#ff4081" dur="4s"
+                  repeatCount="indefinite" />
+              </stop>
+            </linearGradient>
+          </defs>
+
+          <!-- KH Monogram -->
+          <g>
+            <polygon points="10,120 10,20 45,70" fill="url(#gradFinalAnim)">
+              <animateTransform attributeName="transform" type="rotate" from="0 30 70" to="360 30 70" dur="10s"
+                repeatCount="indefinite" />
+            </polygon>
+            <polygon points="45,20 80,120 80,20" fill="url(#gradFinalAnim)" opacity="0.9">
+              <animateTransform attributeName="transform" type="rotate" from="360 62 70" to="0 62 70" dur="12s"
+                repeatCount="indefinite" />
+            </polygon>
+            <polygon points="80,70 115,20 115,120" fill="url(#gradFinalAnim)" opacity="0.7">
+              <animateTransform attributeName="transform" type="rotate" from="0 97 70" to="360 97 70" dur="14s"
+                repeatCount="indefinite" />
+            </polygon>
+          </g>
+
+          <!-- Full Name -->
+          <text x="160" y="85" font-family="'Rubik 80s Fade', system-ui" font-weight="700" font-size="60" fill="#fff">
+            Haitham Portfolio
+          </text>
+        </svg>
+      </v-toolbar-title>
+      <!-- Desktop Menu -->
+      <div class="social-icons">
+        <v-icon v-for="icon in icons" :key="icon" class="icon surprise">
+          {{ icon }}
+        </v-icon>
+      </div>
+      <v-row class="d-none d-md-flex mx-1" align="center" justify="end">
+
+        <v-btn text href="#">Home</v-btn>
+        <v-btn text href="#About">About</v-btn>
+        <v-btn text href="#Skills">Skills</v-btn>
+        <v-btn text href="#Projects">Projects</v-btn>
+        <v-btn text href="#Contact">Contact</v-btn>
+      </v-row>
+
+
+      <!-- Mobile Hamburger -->
+      <v-menu class="d-md-none w-100 w-screen" v-model="drawer" location="end" :close-on-content-click="false">
+        <template #activator="{ props }" class="d-none">
+          <v-btn icon v-bind="props" class="d-md-none">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <div class="w-screen" style="width: 100vw;top:25px !important;position: absolute;
+    width: 100vw;
+    top: 9px !important;
+    right: -52px;">
+
+          <v-list>
+            <v-list-item href="#">Home</v-list-item>
+            <v-list-item href="#About">About</v-list-item>
+            <v-list-item href="#Skills">Skills</v-list-item>
+            <v-list-item href="#Projects">Projects</v-list-item>
+            <v-list-item href="#Contact">Contact</v-list-item>
+          </v-list>
+        </div>
+      </v-menu>
+    </v-app-bar>
+    <v-container class="bg-none">
+      <div ref="vanta" class="vanta-bg" id="vanta"> </div>
+
+    </v-container>
+
+    <myInfoCom class="bg-none fil-content" loading="lazy" />
+    <about class="bg-none fil-content" style="min-height: 100vh;" />
+    <!-- <skills class="bg-none fil-content" /> -->
+    <!-- <projects class="bg-none fil-content" />
+    <contact class="bg-none fil-content" /> -->
+  </v-app>
+</template>
+<style scoped>
+.v-toolbar-title svg {
+  max-width: 120px;
+  height: auto;
+}
+
+
+.v-applictaion {
+  background-color: none !important;
+}
+
+.vanta-bg {
+  width: 100% !important;
+  height: 100vh;
+  position: fixed !important;
+  top: 0;
+  z-index: -1;
+  left: 0;
+
+}
+
+.vanta-canvas {
+  position: fixed !important;
+  width: 100% !important;
+  height: 100% !important;
+}
+
+.v-overlay__content {
+  position: fixed;
+  width: 100% !important;
+  top: 25px !important;
+}
+
+.v-list {
+  width: 100% !important;
+}
+
+.social-icons {
+  display: flex;
+  gap: 24px;
+}
+
+.icon {
+  font-size: 36px;
+  cursor: pointer;
+  animation: surprise 3.5s ease-in-out infinite;
+}
+
+/* delay for surprise effect */
+.icon:nth-child(1) {
+  animation-delay: 0s;
+}
+
+.icon:nth-child(2) {
+  animation-delay: .6s;
+}
+
+.icon:nth-child(3) {
+  animation-delay: 1.2s;
+}
+
+.icon:nth-child(4) {
+  animation-delay: 1.8s;
+}
+
+.icon:nth-child(5) {
+  animation-delay: 2.4s;
+}
+
+.icon {
+  font-size: 20px;
+}
+
+@keyframes surprise {
+  0% {
+    transform: translate(0, 0) rotate(0) scale(1);
+  }
+
+  25% {
+    transform: translate(-6px, -10px) rotate(8deg) scale(1.2);
+  }
+
+  50% {
+    transform: translate(8px, 6px) rotate(-8deg) scale(0.9);
+  }
+
+  75% {
+    transform: translate(-4px, 4px) rotate(4deg) scale(1.15);
+  }
+
+  100% {
+    transform: translate(0, 0) rotate(0) scale(1);
+  }
+}
+</style>
---
+++ src/components/about.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-container class="text-white relative file-content" id="About">
+        <v-row class="align-center justify-space-between absolute fil col-reverse mt-16 mb-16">
+            <h1 class="text-h2 text-center my-2 py-16 w-100">About </h1>
+            <h1 style="padding: 10px;font-size: 20px;line-height: 1.6;">
+                As a performance media buyer, I aim to apply my practical experience in creating data-driven campaigns
+                that
+                deliver real business results. I aim to support a growth-focused team by managing multi-platform paid
+                media,
+                enhancing website performance through CRO and accurate tracking (GTM), and building strong landing page
+                strategies that improve ROI and drive conversions.
+            </h1>
+            <!-- <a href="/Khaled Hassan cv.pdf" download
+                style="color: white; margin:auto;margin-top:50px"><v-icon>mdi-folder</v-icon> Download CV</a> -->
+            <h1 class="text-h2 text-center my-2 py-16 w-100">What I Do?</h1>
+            <ol style="padding: 10px;font-size: 18px;line-height:2; " class="mx-4">
+                <li v-for="n in IDo" :key="n">{{ n }}</li>
+            </ol>
+        </v-row>
+    </v-container>
+
+</template>
+<script>
+export default {
+    name: 'about',
+    data() {
+        return {
+            IDo: {
+                title: "full website Auditing & Conversion Rate Optimization (CRO)",
+                title: "Full Website tracking through GTM & other CMS",
+                title3: "Landing page structure & design",
+                title4: "Create and manage paid media strategies",
+                title5: "A/B testing, optimizing, scaling all paid media campaigns Across Multiple Channels",
+                title6: "proceed on effective ads to drive better fit branding, Conversions, Revenue for the brand"
+            }
+        };
+    },
+}
+</script>
