c0f70cffc12f52c65be13b6d7d04d7ee6b3ca221
5aled1801 <zx5aled1801@gmail.com>
2026-01-30 16:13:10 +0200

    .

--- src/components/about.vue
+++ src/components/about.vue
@@ -14,7 +14,7 @@
                 
                 <h1 class="text-h2 text-center my-2 py-16 w-100 ">What I Do?</h1>
                 <div class="images-wrapper d-flex flex-wrap justify-center align-center">
-                    <img v-for="(img, index) in iDoImages" :key="index" :src="getImage(img)"
+                    <img v-for="(img, index) in IDo" :key="index" :src="getImage(img)"
                         class="what-i-do-img w-50 px-4 bg-white mx-2 my-4 rounded"
                         style="height:190px;object-fit:contain; max-width:350px;  "
                         alt="what i do" />
@@ -47,11 +47,11 @@ export default {
             },
         };
     },
-    computed: {
-        iDoImages() {
-            return Object.values(this.IDo);
-        },
-    },
+    // computed: {
+    //     iDoImages() {
+    //         return Object.values(this.IDo);
+    //     },
+    // },
     methods: {
         getImage(path) {
             return new URL(path, import.meta.url).href;
